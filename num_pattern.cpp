8bfe31d053a6e241870df6ad5a71e578d899c96f
Bhoomika <bhoomika.jalandhra@gmail.com>
2025-06-11 17:16:36 +0530

    Add pattern printing program for fixed number sequence

---
+++ num_pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j,n;
+    for(i=1;i<5;i++)
+    {
+        n=1;
+        for(j=1;j<5;j++)
+        {
+            cout<<n<<" ";
+            n++;
+        }
+        cout<<endl;
+    }
+}
