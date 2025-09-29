3f83c16cbd70681b6153d47ca0b67f0b29048955
Bhoomika <bhoomika.jalandhra@gmail.com>
2025-09-29 18:32:50 +0530

    Add c++ program to convert a number from decimal to binary

---
+++ binary.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+#include<math.h>
+
+int main()
+{
+    int n,bit;
+    cout<<"Enter: ";
+    cin>>n;
+    int ans=0, i=1;
+    while(n!=0)
+    {
+        bit=n&1;
+        ans=(bit*i)+ans;
+        n=n>>1;
+        i*=10;
+    }
+    cout<<ans;
+}
